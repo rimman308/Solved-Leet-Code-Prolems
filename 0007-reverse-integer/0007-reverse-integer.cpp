6be722b3910dfd9a4ec6f2844e954ae7cdcefeff
Rahib Hossain <mail2rahib@gmail.com>
2026-09-05 21:40:01 +0530

    Time: 2 ms (38.38%), Space: 8.6 MB (20.43%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int reverse(int x) {
+        long revNum = 0;
+        while(x!=0){
+            int ld= x%10;
+            revNum=(revNum*10)+ld;
+            x=x/10;
+        }
+        if (revNum > INT_MAX || revNum < INT_MIN) {
+            return 0;
+        }
+        return revNum;
+    }
+};
